044cc4a42a3914565023872a15288a841fc2b562
AntGtz <tranetrose@gmail.com>
2022-05-04 14:43:59 -0500

    testing reader.js

--- test/Reader.test.js
+++ test/Reader.test.js
@@ -1,6 +1,6 @@
 describe("Unidad de pruebas para reader", () => {
     test("1)Verificar que se lea el json", () => {
         const Reader = require("./../lib/utils/Reader");
-        expect(Reader.readJsonFile("./students.json")).not.toBe(undefined);
+        expect(Reader.readJsonFile("./students.json")).not.toBe("[]");
     });
 });
